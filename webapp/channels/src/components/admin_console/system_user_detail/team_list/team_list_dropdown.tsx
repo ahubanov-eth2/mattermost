baf0d62c04b7492ec4f7068cdddeae1a1882cbe0
Aleksandar Hubanov <ahubanov@student.ethz.ch>
2026-01-11 12:58:15 +0200

    Sprout step 1: change class to function + export via memo

--- webapp/channels/src/components/admin_console/system_user_detail/team_list/team_list_dropdown.tsx
+++ webapp/channels/src/components/admin_console/system_user_detail/team_list/team_list_dropdown.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
-import React from 'react';
+import React, { memo } from 'react';
 
 import EllipsisHorizontalIcon from 'components/widgets/icons/ellipsis_h_icon';
 import Menu from 'components/widgets/menu/menu';
@@ -19,66 +19,59 @@ type Props = {
     isDisabled?: boolean;
 }
 
-type State = {
-    serverError: string | null;
-}
+const TeamListDropdown = ({
+    team,
+    doRemoveUserFromTeam,
+    doMakeUserTeamAdmin,
+    doMakeUserTeamMember,
+    isDisabled,
+}: Props) => {
+    const isAdmin = team.scheme_admin;
+    const isMember = team.scheme_user && !team.scheme_admin;
+    const isGuest = team.scheme_guest;
+    const showMakeTeamAdmin = !isAdmin && !isGuest;
+    const showMakeTeamMember = !isMember && !isGuest;
 
-export default class TeamListDropdown extends React.PureComponent<Props, State> {
-    public constructor(props: Props) {
-        super(props);
-
-        this.state = {
-            serverError: null,
-        };
-    }
-
-    public render(): JSX.Element {
-        const {team} = this.props;
-        const isAdmin = team.scheme_admin;
-        const isMember = team.scheme_user && !team.scheme_admin;
-        const isGuest = team.scheme_guest;
-        const showMakeTeamAdmin = !isAdmin && !isGuest;
-        const showMakeTeamMember = !isMember && !isGuest;
-
-        return (
-            <MenuWrapper
-                isDisabled={this.props.isDisabled}
+    return (
+        <MenuWrapper
+            isDisabled={this.props.isDisabled}
+        >
+            <button
+                type='button'
+                id={`teamListDropdown_${team.id}`}
+                className='dropdown-toggle theme color--link style--none'
+                aria-expanded='true'
             >
-                <button
-                    type='button'
-                    id={`teamListDropdown_${team.id}`}
-                    className='dropdown-toggle theme color--link style--none'
-                    aria-expanded='true'
+                <span className='SystemUserDetail__actions-menu-icon'><EllipsisHorizontalIcon/></span>
+            </button>
+            <div>
+                <Menu
+                    openLeft={true}
+                    openUp={false}
+                    ariaLabel={localizeMessage('team_members_dropdown.menuAriaLabel', 'Change the role of a team member')}
                 >
-                    <span className='SystemUserDetail__actions-menu-icon'><EllipsisHorizontalIcon/></span>
-                </button>
-                <div>
-                    <Menu
-                        openLeft={true}
-                        openUp={false}
-                        ariaLabel={localizeMessage('team_members_dropdown.menuAriaLabel', 'Change the role of a team member')}
-                    >
-                        <Menu.ItemAction
-                            id='makeTeamAdmin'
-                            show={showMakeTeamAdmin}
-                            onClick={() => this.props.doMakeUserTeamAdmin(team.id)}
-                            text={localizeMessage('team_members_dropdown.makeAdmin', 'Make Team Admin')}
-                        />
-                        <Menu.ItemAction
-                            show={showMakeTeamMember}
-                            onClick={() => this.props.doMakeUserTeamMember(team.id)}
-                            text={localizeMessage('team_members_dropdown.makeMember', 'Make Team Member')}
-                        />
-                        <Menu.ItemAction
-                            id='removeFromTeam'
-                            show={true}
-                            onClick={() => this.props.doRemoveUserFromTeam(team.id)}
-                            text={localizeMessage('team_members_dropdown.leave_team', 'Remove from Team')}
-                            buttonClass='SystemUserDetail__action-remove-team'
-                        />
-                    </Menu>
-                </div>
-            </MenuWrapper>
-        );
-    }
+                    <Menu.ItemAction
+                        id='makeTeamAdmin'
+                        show={showMakeTeamAdmin}
+                        onClick={() => this.props.doMakeUserTeamAdmin(team.id)}
+                        text={localizeMessage('team_members_dropdown.makeAdmin', 'Make Team Admin')}
+                    />
+                    <Menu.ItemAction
+                        show={showMakeTeamMember}
+                        onClick={() => this.props.doMakeUserTeamMember(team.id)}
+                        text={localizeMessage('team_members_dropdown.makeMember', 'Make Team Member')}
+                    />
+                    <Menu.ItemAction
+                        id='removeFromTeam'
+                        show={true}
+                        onClick={() => this.props.doRemoveUserFromTeam(team.id)}
+                        text={localizeMessage('team_members_dropdown.leave_team', 'Remove from Team')}
+                        buttonClass='SystemUserDetail__action-remove-team'
+                    />
+                </Menu>
+            </div>
+        </MenuWrapper>
+    );
 }
+
+export default memo(TeamListDropdown);
